47eee60ad5b98db51f8f44a67f070b7f9a6972c6
jbr7rr <jbr7rr@users.noreply.github.com>
2024-08-19 17:20:41 +0200

    Initial project setup

---
+++ MedtrumKit/PumpManager/MedtrumPumpManager.swift
@@ -0,0 +1,11 @@
+import HealthKit
+import LoopKit
+import os.log
+
+public class MedtrumPumpManager: DeviceManager {
+    public static let pluginIdentifier = "Medtrum"
+
+    public let localizedTitle = LocalizedString("Medtrum", comment: "Generic title of the Medtrum pump manager")
+
+    // TODO:
+}
